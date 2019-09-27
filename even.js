26abbeaed9c74ff1a406d37040834b0d9e28302e
Dasha Frumovitch <dfrumovitch@gmail.com>
2019-09-27 22:36:04 +0000

    boolean function expression simplification example

---
+++ even.js
@@ -0,0 +1,13 @@
+const isEven  = function (num) {
+  return num % 2 === 0;
+}
+/*
+const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);*/
+
+//by pass the long way and simeply check value when calling the function 
+console.log(isEven(10));
+console.log(isEven(11));
