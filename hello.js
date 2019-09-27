59ab3c6ad8284f12a738d935c3df5a9e9d897c99
Dasha Frumovitch <dfrumovitch@gmail.com>
2019-09-27 22:24:53 +0000

    Playing with Functions

---
+++ hello.js
@@ -0,0 +1,14 @@
+// initial function to set an automated greeting
+const sayHello  = function () {
+  console.log("Hello, puppers and kittens of planet earth!");
+}
+
+sayHello();
+
+// secondary example with parameter
+
+const sayHelloAgain  = function (name) {
+  console.log("Hello dear, " + name);
+}
+sayHelloAgain('Dasha');
+sayHelloAgain('people that are viewing this file.');
